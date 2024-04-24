d1494476798fb9f02bf5d53b477322964326196b
Александр Крюков <dondarubin@gmail.com>
2024-04-24 04:10:38 +0400

    add authMiddleware to profile routes

--- router/index.ts
+++ router/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import UserController from "../controllers/userController";
 import {body} from "express-validator";
 import {authMiddleware} from "../middlewares/authMiddleware";
-import ProfileRepository from "../repositories/profileRepository";
 import ProfileController from "../controllers/profileController";
 
 export const router = express.Router();
@@ -39,12 +38,12 @@ router.post('/login',
   .withMessage("Password must not be empty")
   .isLength({min: 5, max: 20})
   .withMessage("Password must be at least 5 chars long and 20 chars less")
-  .matches(/^[A-Za-z0-9 .,'!&]+$/)
-  .withMessage("Password can consist of latin letters, numbers and special symbols (.,'!&)"),
+  .matches(/^[A-Za-z0-9 .!&]+$/)
+  .withMessage("Password can consist of latin letters, numbers and special symbols (.!&)"),
   UserController.login
 )
 router.post('/logout', UserController.logout)
 router.get('/refresh', UserController.refresh)
 router.get('/users', authMiddleware, UserController.getUsers)
-router.get('/profile/:profileId', ProfileController.getProfile)
-router.put('/profile/:profileId', ProfileController.updateProfile)
+router.get('/profile/:profileId', authMiddleware, ProfileController.getProfile)
+router.put('/profile/:profileId', authMiddleware, ProfileController.updateProfile)
